871e2f72a0950c06bb13b63e02d14187191be8e1
Taran Cacacho <tarancacacho@gmail.com>
2019-09-25 18:53:04 -0700

    Factor out schema to separate file; implement additional resolvers

--- src/index.js
+++ src/index.js
@@ -1,38 +1,48 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
-}]
+}];
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a HackerNews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: (parent, args) => {
+      return links.find(link => link.id === args.id);
+    }
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const linkToUpdate = links.find(link => link.id === args.id);
+      const update = {...args};
+      const updatedLink = {...linkToUpdate, ...update};
+      return updatedLink;
+    }, 
+    deleteLink: (parent, args) => {
+      const linkToDelete = links.find(link => link.id === args.id);
+      links.splice(links.indexOf(linkToDelete), 1);
+      console.log(links);
+      return linkToDelete;
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
